4517604fe665fa8fb35f2c3487e1493a0ea31ed3
mogwai <vincelwq1993@gmail.com>
2018-07-16 21:57:56 +0800

    basic react-webpack-template setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-webpack-template
+
+My personel react-webpack-template.
+
+To use:
+
+1. `npm install`
+2. Add your code in `./src/` folder.
+3. `npm start` to run the dev-server
+4. `npm run build` to generate the build version of your code.
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude:/node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"  //会出现在dist folder
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ]
+}
